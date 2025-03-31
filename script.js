a0a97f9f3070b866f08cb629c3ef66b4c87e973a
Ranbeer Malhotra <ranbeermalhotra2004@gmail.com>
2025-03-31 20:52:04 +0530

    feat: create function getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  switch (choice) {
+    case 0:
+      return "rock";
+    
+    case 1:
+      return "paper";
+
+    case 2:
+      return "scissors";
+  }
+}
