6510b4972101b8521c19314e736e76ac8741600b
Ranbeer Malhotra <ranbeermalhotra2004@gmail.com>
2025-04-17 15:30:48 +0530

    feat(style.css/script.js): style buttons and div.status

--- script.js
+++ script.js
@@ -48,10 +48,14 @@ function watchScoreBoard(humanScore, computerScore, listener) {
     choices.removeEventListener('click', listener);
     const textNode = document.createTextNode(`You Won The Game!`);
     gameScore.appendChild(textNode);
+    gameScore.style.backgroundColor = 'skyblue';
+    gameScore.style.color = 'lightyellow';
   } else if (computerScore.textContent === '5') {
     choices.removeEventListener('click', listener);
     const textNode = document.createTextNode(`You Lose The Game!`);
     gameScore.appendChild(textNode);
+    gameScore.style.backgroundColor = 'red';
+    gameScore.style.color = 'lightyellow';
   }
 }
 
